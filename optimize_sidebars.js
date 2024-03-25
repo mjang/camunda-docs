ac28e00910ec9273b9e90e4bdec348279ba35e00
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-03-25 15:41:57 +0100

    doc(helm-guides): update payment app example

--- optimize_sidebars.js
+++ optimize_sidebars.js
@@ -1848,6 +1848,10 @@ module.exports = {
                 docsLink(
                   "Multi-namespace deployment",
                   "self-managed/platform-deployment/helm-kubernetes/guides/multi-namespace-deployment/"
+                ),                
+                docsLink(
+                  "Verifying Camunda 8 Installation",
+                  "self-managed/platform-deployment/helm-kubernetes/guides/installing-payment-app-example/"
                 ),
               ],
             },
